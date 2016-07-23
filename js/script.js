7597c62734d4ec0aa4b327ea08a962729d17b85d
RaffikaDjuni <raffikarezza27@gmail.com>
2016-07-23 17:10:42 +0700

    Penambahan file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+        function readmore(){
+                var temp=document.getElementById("readmore").style.display;
+                if(temp=="none")
+                    document.getElementById("readmore").style.display="block";
+                else
+                    document.getElementById("readmore").style.display="none";
+        }
